344fd0f9bd13fb274edbfbf3cfa1daf1f9bb622b
Mark Kevin D. Albios <markkevin.usjr@gmail.com>
2018-02-12 17:22:31 +0800

    Added Module for Android Things

---
+++ AppDev/mobile/src/main/java/dev/markie26/androidfaceapp/utils/Util.java
@@ -0,0 +1,13 @@
+package dev.markie26.androidfaceapp.utils;
+
+/**
+ * Created by markie26 on 1/30/18.
+ */
+
+public class Util {
+
+    //KAIROS API KEYS
+    public static final String APP_ID = "b1e6039d";
+    public static final String API_KEY = "c9f73ac08aa98541f6e78f8d8b68c0bc";
+
+}
